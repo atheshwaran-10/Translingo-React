7562f0c300c685b5354565955f51f465b840f16f
Atheshwaran K <athesh85@gmail.com>
2023-08-26 13:56:51 +0530

    Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      external: ['lodash'],
+    },
+  },
+};
